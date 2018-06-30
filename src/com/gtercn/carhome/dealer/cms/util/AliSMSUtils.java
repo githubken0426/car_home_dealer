6b450f9b7d4f89a0afa3e4eda1daf7ae66257e22
Administrator <Administrator@cm-PC>
2018-06-30 10:36:29 +0800

    20180630

--- src/com/gtercn/carhome/dealer/cms/util/AliSMSUtils.java
+++ src/com/gtercn/carhome/dealer/cms/util/AliSMSUtils.java
@@ -2,8 +2,6 @@ package com.gtercn.carhome.dealer.cms.util;
 
 import com.aliyuncs.DefaultAcsClient;
 import com.aliyuncs.IAcsClient;
-import com.aliyuncs.exceptions.ClientException;
-import com.aliyuncs.exceptions.ServerException;
 import com.aliyuncs.profile.DefaultProfile;
 import com.aliyuncs.profile.IClientProfile;
 import com.aliyuncs.sms.model.v20160927.SingleSendSmsRequest;
@@ -12,45 +10,6 @@ import com.gtercn.carhome.dealer.cms.ApplicationConfig;
 
 
 public class AliSMSUtils {
-	public static void main(String[] args) {
-		String mobile = "18642690085";
-		// String mobile = "15840602341";
-		//sendUserDealerMsg(mobile, "order_002", "2018-04-25",  "小波货站","2018-04-29");
-		sendDelaerMsg(mobile,"lisi","order_002", "2018-04-25");
-		//sendDealerShippedMsg(mobile, "order_002", "2018-04-25",  "小波货站");
-		//sendUserSelfMsg(mobile, "order_002", "2018-04-25");
-	}
-	/**
-	 * 验证码短信
-	 * @param mobile
-	 * @param tempCode
-	 * @param validateCode
-	 * @throws 
-	 * @date 2018年4月28日 下午12:56:25
-	 */
-	public static void sendVerifyCodeMsg(String mobile, String tempCode, String validateCode) {
-		try {
-			String signName = ApplicationConfig.MESSAGE_SIGN;// 签名名称
-			IClientProfile profile = DefaultProfile.getProfile("cn-hangzhou", ApplicationConfig.MESSAGE_ACCESS_KEY,
-					ApplicationConfig.MESSAGE_ACCESS_SECRET);
-			DefaultProfile.addEndpoint("cn-hangzhou", "cn-hangzhou", "Sms", "sms.aliyuncs.com");
-			IAcsClient client = new DefaultAcsClient(profile);
-			SingleSendSmsRequest request = new SingleSendSmsRequest();
-			request.setSignName(signName);// 控制台创建的签名名称
-			request.setTemplateCode(tempCode);// 控制台创建的模板CODE
-			// 短信模板中的变量；数字需要转换为字符串；个人用户每个变量长度必须小于15个字符。
-			request.setParamString("{\"code\":\"" + validateCode + "\",\"product\":\"" + signName + "\"}");
-			// request.setParamString("{}");
-			request.setRecNum(mobile);// 接收号码
-			SingleSendSmsResponse httpResponse = client.getAcsResponse(request);
-			System.out.println(httpResponse);
-		} catch (ServerException e) {
-			e.printStackTrace();
-		} catch (ClientException e) {
-			e.printStackTrace();
-		}
-	}
-
 	/**
 	 * 用户个人接收短息
 	 * @param mobile
